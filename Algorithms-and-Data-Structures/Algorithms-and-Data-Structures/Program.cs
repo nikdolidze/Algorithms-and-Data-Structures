d535cd47e6aaa3f4edba761d8f8a4d9e786507ad
Nick Dolidze <N.Dolidze@Tbcpay.ge>
2022-04-27 17:17:36 +0400

    Added initial Project

---
+++ Algorithms-and-Data-Structures/Algorithms-and-Data-Structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms_and_Data_Structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
