510c64cbd0a7aa97c81dd017bff5082a96ab7495
Nick Dolidze <N.Dolidze@Tbcpay.ge>
2022-04-28 13:47:48 +0400

    Added exercises link

--- Algorithms-and-Data-Structures/Algorithms-and-Data-Structures/Program.cs
+++ Algorithms-and-Data-Structures/Algorithms-and-Data-Structures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithms_and_Data_Structures.Basic;
+//https://www.w3resource.com/csharp-exercises/basic/index.php  exercises link
 namespace Algorithms_and_Data_Structures
 {
     internal class Program
