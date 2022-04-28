12f15040e44963fd9999e63bf1fe959b7aeceeb0
Nick Dolidze <N.Dolidze@Tbcpay.ge>
2022-04-28 13:04:44 +0400

    swap two numbers

---
+++ Algorithms-and-Data-Structures/Algorithms-and-Data-Structures/Basic/Swap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms_and_Data_Structures.Basic
+{
+    // Write a C# Sharp program to swap two numbers
+    public static class Swap
+    {
+        public static void SwapExample()
+        {
+            Console.WriteLine("Input the First Number ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Second Number ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            secondNumber = secondNumber + firstNumber;
+            firstNumber = secondNumber - firstNumber;
+            secondNumber = secondNumber - firstNumber;
+
+            var memp = secondNumber;
+            var second = firstNumber;
+            var first = memp;
+
+            Console.WriteLine("first number : " + firstNumber);
+            Console.WriteLine("second number : " + secondNumber);
+        }
+    }
+}
--- Algorithms-and-Data-Structures/Algorithms-and-Data-Structures/Program.cs
+++ Algorithms-and-Data-Structures/Algorithms-and-Data-Structures/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
+﻿using Algorithms_and_Data_Structures.Basic;
 namespace Algorithms_and_Data_Structures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Swap.SwapExample();
         }
     }
 }
